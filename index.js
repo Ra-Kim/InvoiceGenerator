c641f0ce2fa0365a3af3cf231a75c4fdb30e7665
Okorie Emmanuella <ellahokorie@gmail.com>
2022-04-14 18:25:23 +0200

    worked on disabling buttons dynamically

--- index.js
+++ index.js
@@ -1,123 +1,154 @@
 let button = [
-    one ={
+  (one = {
     name: "Wash Car",
-    price: 10
-    },
-    two={
-    name : "Mow Lawn",
-    price: 20
-    },
-    three = {
+    price: 10,
+    state: false,
+  }),
+  (two = {
+    name: "Mow Lawn",
+    price: 20,
+    state: false,
+  }),
+  (three = {
     name: "Pull Weeds",
-    price: 30
-    }
-]
+    price: 30,
+    state: false,
+  }),
+];
 
-window.onload = function buttonDisplay(){
-    let ulEl = document.getElementById("ulEl")
-    let taskList = document.getElementById("task-list")
-    let totalSum = document.getElementById("total-price")
-    let spanNote = document.getElementById("notes-span")
-    let taskArray = []
-    let elemArray = []
-    let tasks = ""
-    let buttons=""
-    let localArray =[]
-    let sum = 0
-    for(let i =0; i<button.length;i++){
-        buttons += `<li><button class = "task-button" id = "task-button${i+1}">${button[i].name}:  $${button[i].price}</button></li>`
-    }
-    ulEl.innerHTML=buttons
+let buttonList = [
+  {
+    id: "task-button1",
+  },
+  {
+    id: "task-button2",
+  },
+  {
+    id: "task-button3",
+  },
+];
 
-    let tasksFromLocalStorage = JSON.parse( localStorage.getItem("tasks") )
-    let particular = JSON.parse( localStorage.getItem("particular") )
-    let storedSum = localStorage.getItem("totalSum")
+let ulEl = document.getElementById("ulEl");
+let taskList = document.getElementById("task-list");
+let totalSum = document.getElementById("total-price");
+let spanNote = document.getElementById("notes-span");
+let taskArray = [];
+let elemArray = [];
+let tasks = "";
+let buttons = "";
+let localArray = [];
+let sum = 0;
+const setBtnIDToLocalStorage = (button) => {
+  localStorage.setItem(button, true);
 
-    
-    if (tasksFromLocalStorage){
-        localArray=tasksFromLocalStorage
-        for(let i =0; i<localArray.length;i++){
-            tasks += localArray[i]
+  document.getElementById(button).setAttribute("disabled", true);
+};
+const isBtnDisabled = (button) => {
+  if (localStorage.getItem(button)) {
+    document.getElementById(button).setAttribute("disabled", true);
+  }
+};
+
+window.onload = function buttonDisplay() {
+  //   for (let i = 0; i < button.length; i++) {
+  //     buttons += `<li><button class = "task-button" id = "task-button${i + 1}">${
+  //       button[i].name
+  //     }:  $${button[i].price}</button></li>`;
+  //   }
+  //   ulEl.innerHTML = buttons;
+
+  // const buttonList = button
+  //   .map((button, i) => {
+  //     return `<li><button class = "task-button" >${button.name}:  $${button.price}</button></li>`;
+  //   })
+  //   .join("");
+  // ulEl.innerHTML = buttonList;
+
+  let particular = JSON.parse(localStorage.getItem("particular"));
+
+  document.querySelectorAll(".task-button").forEach((elem) => {
+    isBtnDisabled(elem.id);
+    elem.addEventListener("click", function () {
+      if (particular == null || !particular.includes(elem)) {
+        if (elemArray == null || !elemArray.includes(elem)) {
+          processButton(elem);
+          spanNote.textContent = "We accept cash, credit card, or PayPal";
         }
-        taskList.innerHTML = tasks
-        totalSum.textContent = "$" + storedSum
-        spanNote.textContent = "We accept cash, credit card, or PayPal"
-        tasks = ""
-        }
-    
+        elemArray.push(elem);
+        setBtnIDToLocalStorage(elem.id);
+        console.log(elem, "elem");
 
-    
+        localStorage.setItem("particular", JSON.stringify(elemArray));
+      }
+    });
+  });
 
-    document.querySelectorAll(".task-button").forEach(elem =>{
-        elem.addEventListener("click", function(){
-            if(particular == null || !particular.includes(elem)){
-                if(elemArray == null || !elemArray.includes(elem)){
-                    processButton(elem)
-                    spanNote.textContent = "We accept cash, credit card, or PayPal"
-                }
-                elemArray.push(elem)
-                localStorage.setItem("particular", JSON.stringify(elemArray))
-            }
-        })
-
-        function processButton(button) {
-            tasks = ""
-            const values = button.innerHTML.split(" ");
-            let taskName = values[0] +" "+ values[1].replace(":",'')
-            let taskPrice = values[3].replace ( /[^\d.]/g, '' )
-            let taskLiteral = `<li>
+  function processButton(button) {
+    tasks = "";
+    const values = button.innerHTML.split(" ");
+    let taskName = values[0] + " " + values[1].replace(":", "");
+    let taskPrice = values[3].replace(/[^\d.]/g, "");
+    let taskLiteral = `<li>
                                 <div><p class = "task-name" id = "task">${taskName} <span class = "delete-span">(remove)</span><p></div>
                                 <div> <p id ="task-price"><span id = "price-span">$</span>${taskPrice}<p></div>
-                              </li>`
+                              </li>`;
 
-            if(tasksFromLocalStorage){
-                taskArray = localArray
-            }
+    if (tasksFromLocalStorage) {
+      taskArray = localArray;
+    }
 
-            taskArray.push(taskLiteral)
-            localStorage.setItem("tasks", JSON.stringify(taskArray) )
-            let storedSum = localStorage.getItem("totalSum")
-            
-            if(!tasksFromLocalStorage){
-                for(let i =0; i<taskArray.length;i++){
-                    tasks += taskArray[i]
-                }
-            sum += Number(taskPrice) 
+    taskArray.push(taskLiteral);
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+    let storedSum = localStorage.getItem("totalSum");
 
-            }else{
-                for(let i =0; i<localArray.length;i++){
-                    tasks += localArray[i]
-                }
-            sum = Number(taskPrice) + Number(storedSum)
+    if (!tasksFromLocalStorage) {
+      for (let i = 0; i < taskArray.length; i++) {
+        tasks += taskArray[i];
+      }
+      sum += Number(taskPrice);
+    } else {
+      for (let i = 0; i < localArray.length; i++) {
+        tasks += localArray[i];
+      }
+      sum = Number(taskPrice) + Number(storedSum);
+    }
 
-            }
-                
-            // sum = Number(taskPrice) + storedSum
-            localStorage.setItem ("totalSum", sum )
-            totalSum.textContent = "$" + sum
-            taskList.innerHTML = tasks
-        }
-    })
+    // sum = Number(taskPrice) + storedSum
+    localStorage.setItem("totalSum", sum);
+    totalSum.textContent = "$" + sum;
+    taskList.innerHTML = tasks;
+  }
 
-    document.getElementById("reset-button").addEventListener("click", function(){
-        tasks = ""
-        localStorage.clear("tasks")
-        localStorage.clear("totalSum")
-        localStorage.clear("particular")
-        taskArray = []
-        elemArray = []
-        localArray =[]
-        taskList.innerHTML = ""
-        spanNote.textContent = ""
-        sum = 0
-        totalSum.textContent = "$0"
-        document.querySelectorAll(".task-button").forEach(elem =>{
-                elem.disabled = false
-        })
-    })
-    
-}
+  let tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+  let storedSum = localStorage.getItem("totalSum");
 
+  if (tasksFromLocalStorage) {
+    localArray = tasksFromLocalStorage;
+    for (let i = 0; i < localArray.length; i++) {
+      tasks += localArray[i];
+    }
+    taskList.innerHTML = tasks;
+    totalSum.textContent = "$" + storedSum;
+    spanNote.textContent = "We accept cash, credit card, or PayPal";
+    tasks = "";
+  }
 
-
-
+  document
+    .getElementById("reset-button")
+    .addEventListener("click", function () {
+      tasks = "";
+      localStorage.clear("tasks");
+      localStorage.clear("totalSum");
+      localStorage.clear("particular");
+      taskArray = [];
+      elemArray = [];
+      localArray = [];
+      taskList.innerHTML = "";
+      spanNote.textContent = "";
+      sum = 0;
+      totalSum.textContent = "$0";
+      document.querySelectorAll(".task-button").forEach((elem) => {
+        elem.disabled = false;
+      });
+    });
+};
